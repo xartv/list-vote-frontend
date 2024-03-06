bc4c97a0a45c158a5e1a7eef30e459f990c1938f
Artem Vasilev <xartvdev@gmail.com>
2024-03-06 23:59:57 +0400

    base config for next app

---
+++ src/config/pages-url.config.ts
@@ -0,0 +1,9 @@
+class DASHBOARD {
+  private root = '/d';
+
+  HOME = this.root;
+  CREATED_LISTS = `${this.root}/created-lists`;
+  ACCESSED_LISTS = `${this.root}/accessed-lists`;
+}
+
+export const DASHBOARD_PAGES = new DASHBOARD();
