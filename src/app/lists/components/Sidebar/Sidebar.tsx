0ade7ddc8458039d983d0e3fbd0c61408f8af1b9
Artem Vasilev <xartvdev@gmail.com>
2024-04-23 23:56:08 +0400

    remove console log

--- src/app/lists/components/Sidebar/Sidebar.tsx
+++ src/app/lists/components/Sidebar/Sidebar.tsx
@@ -8,8 +8,6 @@ import { useLists } from '@/hooks/useLists';
 export function Sidebar() {
   const { data: lists } = useLists();
 
-  console.log(lists);
-
   return (
     <section className='flex h-full w-[286px] flex-col gap-[16px]'>
       <Button>Создать заметку</Button>
